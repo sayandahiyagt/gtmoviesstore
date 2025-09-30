2798ad7d21a31d7f4d2c234620373b5314b29cd7
Sayan Dahiya <sayan.dahiya@gmail.com>
2025-09-30 12:51:08 -0400

    Feature:petition added

--- GTmoviesstore/settings.py
+++ GTmoviesstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8u0)it%&xk(4j$5g3@339c7ib9de^g1rrzdf*h@mah-vn%==fv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
